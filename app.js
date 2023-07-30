b0767d4fcb4c3474c44e9e31ad763b07a9685736
junaidsalam639 <junaidsalam639@gmail.com>
2023-07-30 15:00:13 +0500

    'first'

--- app.js
+++ app.js
@@ -7,8 +7,15 @@ document.getElementById('add').addEventListener('click', async () => {
     try {
         const docRef = await addDoc(collection(db, "TodoList"), {
             input: input.value
-        });
-        location.reload()
+        })
+        Swal.fire({
+            icon: 'success',
+            title: 'Todo Add...',
+            text: 'TodoAdd Successfully!',
+            footer: '<a href="">Why do I have this issue?</a>'
+          }).then(()=>{
+              location.reload()
+          })
         console.log("Document written with ID: ", docRef.id);
     } catch (e) {
         console.error("Error adding document: ", e);
@@ -22,8 +29,8 @@ querySnapshot.forEach((doc) => {
     let list = document.getElementById('list').innerHTML +=
         `<li>
         <span>${doc.data().input}</span>
-        <button onclick="edit('${doc.id}')">Edit</button>
-        <button onclick="dele('${doc.id}')">Delete</button>
+        <button onclick="edit('${doc.id}')"><i class='fas fa-edit'></i>Edit</button>
+        <button onclick="dele('${doc.id}')"><i class='fas fa-trash'></i>Delete</button>
      </li>`
 });
 
@@ -37,17 +44,34 @@ async function edit(e) {
     await updateDoc(washingtonRef, {
         input: pro,
     });
-    location.reload()
+    Swal.fire({
+        icon: 'success',
+        title: 'Edit...',
+        text: 'Edit Successfully!',
+        footer: '<a href="">Why do I have this issue?</a>'
+      }).then(()=>{
+          location.reload()
+      })
 }
 
 // Delete code
 async function dele(e) {
     console.log(e);
     await deleteDoc(doc(db, "TodoList", e));
-    location.reload()
+    Swal.fire({
+        icon: 'success',
+        title: 'Deleted...',
+        text: 'Deleted Successfully!',
+        footer: '<a href="">Why do I have this issue?</a>'
+      }).then(()=>{
+          location.reload()
+        })
 }
 
 
 
 window.edit = edit
 window.dele = dele
+
+
+
